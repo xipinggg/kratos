21731e3aee61c1e7712cbd11adb91374aad67f80
xpgg <992212552@qq.com>
2023-08-06 00:44:52 +0800

    [ADD] errors包增加WithStack,WithMessage函数

--- errors/with.go
+++ errors/with.go
@@ -5,17 +5,17 @@ import (
 	"runtime/debug"
 )
 
-func WithStack(err error) error {
-	if err == nil {
-		return nil
-	}
-	return fmt.Errorf("%w\nstack: %s", err, debug.Stack())
-}
-
 func WithMessage(err error, format string, args ...any) error {
 	if err == nil {
 		return nil
 	}
 	msg := fmt.Sprintf(format, args...)
 	return fmt.Errorf("%s\n%w", msg, err)
+}
+
+func WithStack(err error) error {
+	if err == nil {
+		return nil
+	}
+	return fmt.Errorf("%w\nstack: %s", err, debug.Stack())
 }
